701754a7ebd49ac87d90807c79f3225412d3feb7
ChrisMisztur <cmisztur@mriiot.com>
2024-10-12 22:47:48 -0500

    * build image

--- make_image.sh
+++ make_image.sh
@@ -4,14 +4,16 @@ script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path"
 
 mkdir build
-
 source esp-idf/export.sh
 cd micropython/ports/esp32
+
+make submodules
 make BOARD=ESP32_GENERIC_S3 clean
+
 idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER build
-idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER size
-idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER size-components
-idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER size-files
+# idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER size
+# idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER size-components
+# idf.py -D MICROPY_BOARD=ESP32_GENERIC_S3 -B build-WALTER size-files
 
 cp ./build-WALTER/sdkconfig.combined ../../../build/sdkconfig.combined
 cp ./build-WALTER/bootloader/bootloader.bin ../../../build/bootloader.bin
---
+++ write_stock_image.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export ESPTOOL_PORT=/dev/tty.usbmodem14201
+
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$script_path"
+
+curl https://micropython.org/resources/firmware/ESP32_GENERIC_S3-20240602-v1.23.0.bin -o firmware.bin
+
+esptool.py --chip esp32s3 flash_id
+
+esptool.py --chip esp32s3 erase_flash
+
+esptool.py --chip esp32s3 write_flash -z 0 firmware.bin
